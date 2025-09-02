48d7933109c345f60cdf67b04a1d1ff7bff99dfa
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2025-09-02 19:35:20 +0200

    feat: the stored values can now be retrieved by their key

--- main.c
+++ main.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 
 char* filePath = "C:\\Users\\Gabriel\\desktop\\notes.txt";
 FILE* fptr;
 
-void addKeyValuePair(const char* key, const char* value);
+void setKeyValuePair(const char* key, const char* value);
+char* getKeyValue(const char* key);
 
 int main(void){
-    printf("Welcome to Notes CLI!");
-    addKeyValuePair("1", "add sound effects to Jufofu promo video");
+    printf("Welcome to Notes CLI!\n");
+    setKeyValuePair("1", "add sound effects to Jufofu promo video");
+    char* result = getKeyValue("1");
+    printf("%s\n", result);
 }
 
-void addKeyValuePair(const char* key, const char* value){
+void setKeyValuePair(const char* key, const char* value){
     fptr = fopen(filePath, "a");
 
     if(fptr == NULL){
-        printf("There was an error while attempting to open the file.\n");
+        printf("Error while attempting to open the file.\n");
         return;
     }
 
     fprintf(fptr, "%s=%s\n", key, value); 
     fclose(fptr);
 }
+
+char* getKeyValue(const char* searchKey){
+    fptr = fopen(filePath, "r");
+
+    if(fptr == NULL){
+        printf("Error while attempting to open the file.\n");
+        return NULL;
+    }
+
+    char line[256];
+
+
+    while(fgets(line, sizeof(line), fptr)){
+        char* key = strtok(line, "=");
+        char* value = strtok(NULL, "=");
+
+        if(strcmp(key, searchKey) == 0){
+            char* val = strdup(value);
+            fclose(fptr);
+            return val;
+        }
+    }
+
+    fclose(fptr);
+};
